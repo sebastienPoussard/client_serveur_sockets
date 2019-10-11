1d18aefaf7df684548ad9370a51c2a7e8e5fe401
Seb <s>
2019-10-11 18:59:25 +0200

    Add color for messages + Beginning working on response to client

--- serveur.c
+++ serveur.c
@@ -12,12 +12,24 @@
 #include <signal.h>
 #include "lib.h"
 
+// couleurs pour le terminal
+#define RED   "\x1B[31m"
+#define GRN   "\x1B[32m"
+#define YEL   "\x1B[33m"
+#define BLU   "\x1B[34m"
+#define MAG   "\x1B[35m"
+#define CYN   "\x1B[36m"
+#define WHT   "\x1B[37m"
+#define RESET "\x1B[0m"
+
 int main() {
 
     // ==================================== VARIABLES ====================================================
     char port[5];     			        // port définis par l'utilisateur
-    int soc;		        	        // descripteur de socket
-    char adresseCli[50];                // adresse IP du client
+    int socEcoute;	        	        // descripteur de socket en écoute de requêtes
+    int socRep;                         // descripteur de socket en réponse aux requêtes
+    char ipClient[1024];                // adresse IP du client
+    char service[20];
     int tailleDonnees;                  // taille de données reçues
     int tcli;                           // taille de la structure du client qui envoie des données
     struct sockaddr_storage addrClient; // structure qui contient les informations du client
@@ -35,19 +47,30 @@ int main() {
 
     // ================================== TRAITEMENT DES DONNEES RECUES  ======================================= 
     // création de le socket d'écoute
-    soc = creationSocketDgram(NULL,port);
+    socEcoute = creationSocketDgram(NULL,port);
     tcli = sizeof addrClient;
     // nettoyer le buffer
     memset(buffer, 0, sizeof buffer);
     while(1) {
         printf("En attente de reception d'un message ...\n");
-        if ((tailleDonnees = recvfrom(soc, buffer, bufferMAX-1 , 0, (struct sockaddr *)&addrClient, &tcli)) == -1) {
+        if ((tailleDonnees = recvfrom(socEcoute, buffer, bufferMAX-1 , 0, (struct sockaddr *)&addrClient, &tcli)) == -1) {
             perror("recvfrom");
             exit(1);
         }    
-        printf("J'ai reçue : %s \n",buffer); 
-        // création d'une socket dgram pour envoie de données
-
+        // récuperer l'ip du client et le port du client
+        getnameinfo(&addrClient, sizeof addrClient, ipClient, sizeof ipClient, service, sizeof service, 0);
+        printf("Message reçue de ");
+        printf(BLU "%s:%s",ipClient,service);
+        printf(RESET " --> ");
+        printf(MAG "%s\n" RESET,buffer);
+        // création d'une socket dgram pour répondre au client
+        socRep = creationSocketDgram(ipClient,service);
+        // envoi de données au client
+        int numbytes;
+        if ((numbytes = sendto(socRep, buffer, strlen(buffer), 0, ipClient, service)) == -1) {
+            perror("talker: sendto");
+            exit(1);
+        }
         // nettoyer le buffer pour la prochaine reception
         memset(buffer, 0, sizeof buffer);
     }
