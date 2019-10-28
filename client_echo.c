6ad759aff9c2f3beb3fa5ce5bee4637141d7075c
Bredariol <romain.bredariol@gmail.com>
2019-10-28 14:59:24 +0100

    envoie avec lib ok, ecoute à tester

--- client_echo.c
+++ client_echo.c
@@ -8,6 +8,7 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <netdb.h>
+#include "lib.h"
 
 
 int main (){
@@ -30,76 +31,44 @@ int main (){
 	printf("message a envoyer au serveur :");
 	scanf("%s", msg);
 
-	memset(&hints, 0, sizeof hints);
-	hints.ai_family = AF_UNSPEC;
-	hints.ai_socktype = SOCK_DGRAM;
+	socket_envoie = socDgramEnvoie(adresse, port, msg);
 
-	if ((rv = getaddrinfo(adresse, port, &hints, &servinfo)) != 0) {
-		fprintf(stderr, "getaddrinfo: %s\n", strerror(rv));
-		return 1;
-	}
+	////reception
+	//memset(&hints, 0, sizeof hints);
+	//hints.ai_family = AF_UNSPEC;
+	//hints.ai_socktype = SOCK_DGRAM;
 
-	for(p = servinfo; p != NULL; p = p->ai_next) {
-		if ((socket_envoie = socket(p->ai_family, p->ai_socktype,
-						p->ai_protocol)) == -1) {
-			perror("talker: socket");
-			continue;
-		}
+	//if ((rv = getaddrinfo(adresse, port, &hints, &servinfo)) != 0) {
+	//	fprintf(stderr, "getaddrinfo: %s\n", strerror(rv));
+	//	return 1;
+	//}
 
-		break;
-	}
+	//for(p = servinfo; p != NULL; p = p->ai_next) {
+	//	if ((socket_envoie = socket(p->ai_family, p->ai_socktype,
+	//					p->ai_protocol)) == -1) {
+	//		perror("talker: socket");
+	//		continue;
+	//	}
 
-	if (p == NULL) {
-		fprintf(stderr, "talker: failed to create socket\n");
-		return 2;
-	}
+	//	break;
+	//}
 
-	if ((numbytes = sendto(socket_envoie, msg, strlen(msg), 0,
-					p->ai_addr, p->ai_addrlen)) == -1) {
-		perror("talker: sendto");
-		exit(1);
-	}
+	//if (p == NULL) {
+	//	fprintf(stderr, "talker: failed to create socket\n");
+	//	return 2;
+	//}
 
-	freeaddrinfo(servinfo);
+	//getnameinfo(&addrServeur, sizeof addrServeur, adresse, sizeof adresse, port, sizeof port, 0);
 
-	close(socket_envoie);
+	//if ((numbytes = recvfrom(socket_ecoute, msg, bufferMAX-1 , 0,
+	//				(struct sockaddr *)&addrServeur, sizeof(addrServeur))) == -1) {
+	//	perror("recvfrom");
+	//	exit(1);
+	//}	
 
-	//reception
-	memset(&hints, 0, sizeof hints);
-	hints.ai_family = AF_UNSPEC;
-	hints.ai_socktype = SOCK_DGRAM;
+	//freeaddrinfo(servinfo);
 
-	if ((rv = getaddrinfo(adresse, port, &hints, &servinfo)) != 0) {
-		fprintf(stderr, "getaddrinfo: %s\n", strerror(rv));
-		return 1;
-	}
-
-	for(p = servinfo; p != NULL; p = p->ai_next) {
-		if ((socket_envoie = socket(p->ai_family, p->ai_socktype,
-						p->ai_protocol)) == -1) {
-			perror("talker: socket");
-			continue;
-		}
-
-		break;
-	}
-
-	if (p == NULL) {
-		fprintf(stderr, "talker: failed to create socket\n");
-		return 2;
-	}
-
-	getnameinfo(&addrServeur, sizeof addrServeur, adresse, sizeof adresse, port, sizeof port, 0);
-
-	if ((numbytes = recvfrom(socket_ecoute, msg, bufferMAX-1 , 0,
-					(struct sockaddr *)&addrServeur, sizeof(addrServeur))) == -1) {
-		perror("recvfrom");
-		exit(1);
-	}	
-
-	freeaddrinfo(servinfo);
-
-	close(socket_envoie);
+	//close(socket_envoie);
 
 
 	return 0;
--- lib.h
+++ lib.h
@@ -4,3 +4,15 @@
 // sur un port donné
 int creationSocketDgram(char adresse[50], char port[5]);
 
+// cette fonction créer et renvoie une socket DGRAM en écoute.
+// la socket sera ouverte sur le port spécifié en parametre.
+int socDgramEcoute(char port[5]) ;
+
+// cette fonction créer une socket DATAGRAM et envoie un message
+// les parametres adresse et port permettent de spécifier où envoyer le message
+// le parametre message contient le message à envoyer limité à 1024 caractères
+// la fonction renvoie 0 en cas de succès
+int socDgramEnvoie(char adresse[50], char port[5], char message[1024]); 
+
+
+
